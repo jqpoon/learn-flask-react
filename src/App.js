2b39e138e3c2c928fc576cf22d468c9514223e7a
Jia Qi Poon <poonjiaqi@gmail.com>
2021-05-31 05:11:10 +0800

    Update react frontend to parse data from backend

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  /* Returns a getter and setter for this state. */
+  const [currentTime, setCurrentTime] = useState(0);
+
+  useEffect(() => {
+    fetch('/time').then(res => res.json())
+                  .then(data => {setCurrentTime(data.time);
+    });
+  }, []); // Empty list here so that this function only called on initial rendering
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,7 +27,9 @@ function App() {
         >
           Learn React
         </a>
+        <p>The current time is {currentTime}.</p>
       </header>
+
     </div>
   );
 }
